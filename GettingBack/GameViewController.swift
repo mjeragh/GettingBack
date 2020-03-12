3a5413ed19abefdb1d319e38ee14a8ee60112bd7
Mohammad Jeragh <mjeragh@gmail.com>
2020-03-12 23:33:00 +0300

    Initial Commit

---
+++ GettingBack/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  GettingBack
+//
+//  Created by Mohammad Jeragh on 3/12/20.
+//  Copyright © 2020 Mohammad Jeragh. All rights reserved.
+//
+
+import UIKit
+import MetalKit
+
+// Our iOS specific view controller
+class GameViewController: UIViewController {
+
+    var renderer: Renderer!
+    var mtkView: MTKView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let mtkView = view as? MTKView else {
+            print("View of Gameview controller is not an MTKView")
+            return
+        }
+
+        // Select the device to render with.  We choose the default device
+        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
+            print("Metal is not supported")
+            return
+        }
+        
+        mtkView.device = defaultDevice
+        mtkView.backgroundColor = UIColor.black
+
+        guard let newRenderer = Renderer(metalKitView: mtkView) else {
+            print("Renderer cannot be initialized")
+            return
+        }
+
+        renderer = newRenderer
+
+        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+
+        mtkView.delegate = renderer
+    }
+}
